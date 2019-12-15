90dd8bd1d11bb76b6a2720219716f068f68a1460
chentao <384460967@qq.com>
2019-12-15 13:06:07 +0800

    初始化git项目

---
+++ src/main/java/com/software/gameforum/GameforumApplication.java
@@ -0,0 +1,13 @@
+package com.software.gameforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameforumApplication.class, args);
+    }
+
+}
